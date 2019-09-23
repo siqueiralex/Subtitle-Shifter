009d13ed867aa10c19bc3fd4668e29e0953707f1
Alex Siqueira Lacerda <Lequizi@gmail.com>
2019-09-23 01:59:34 -0300

    final

--- subshifter.py
+++ subshifter.py
@@ -3,37 +3,65 @@ import sys
 import datetime
 import os
 
-###################################
-#   USAGE:
-#   -$ python3 subshifter location.srt destination.srt [delta in milisseconds] [optional encoding]
-#   Example: python subshifter subtitles.srt shiftedsubtitles -1000 latin
+# Args handling
+if (len(sys.argv) < 3 or len(sys.argv) > 4):
+    print(r'subshifter usage: subshifter subtitles.srt [delta in milisseconds] [encoding (optional)]')
+    print("Please try again...")
+    sys.exit(0)
 
 
+if (sys.argv[1][-4:] != ".srt") :
+    print("ERROR: entry file must be a .srt subtitle")
+    print(r'subshifter usage: subshifter subtitles.srt [delta in milisseconds] [encoding (optional)]')
+    print("Please try again...")
+    sys.exit(0)
 
-if (len(sys.argv) < 4 or len(sys.argv) > 5):
-    print("fez merda ai")
-    sys.exit(-1)
-
+#  Default encoding = latin
 encoding = 'latin'
 if len(sys.argv) == 5:
-    encoding = sys.argv[4]
+    encoding = sys.argv[3]
 
 
+# Args reading
 file = open(os.path.join(os.getcwd(),sys.argv[1]), 'r', encoding=encoding)
-out_file = open(os.path.join(os.getcwd(),sys.argv[2]),'w',encoding=encoding)
-delta = int( sys.argv[3])
+out_buff = []
+
+# Checking if delta is a valid numbe r
+try:
+    delta = int(sys.argv[2])
+except ValueError:
+    print("ERROR: delta must be an integer number")
+    print(r'subshifter usage: subshifter subtitles.srt [delta in milisseconds] [encoding (optional)]')
+    print("Please try again...")
+    sys.exit(0)
+
+
+
+print("Reading and Shifting subtitles in {} milisseconds...".format(delta))
 
 lines = file.read().splitlines()
 
 for line in lines:
     new_line = line
-    results = re.finditer(r'\d\d:\d\d:\d\d,\d{3}', new_line) 
-    for result in results:
-        hora = result.group()
-        time = datetime.datetime.strptime(hora, '%H:%M:%S,%f')
-        new_time = time + datetime.timedelta(milliseconds=delta)
-        new_line = new_line.replace(result.group(),str(new_time).split()[1].replace('.',',')[0:-3])
 
-    out_file.write(new_line)
-    out_file.write("\n")
-out_file.close()
+    # Looking for time marks by using regex
+    results = re.finditer(r'\d\d:\d\d:\d\d,\d{3}', new_line) 
+
+    # iterating through time marks and shifting
+    for result in results:
+        second = result.group()
+        time = datetime.datetime.strptime(second, '%H:%M:%S,%f')
+        new_time = time + datetime.timedelta(milliseconds=delta)
+        new_line = new_line.replace(second, str(new_time).split()[1].replace('.',',')[0:-3])
+
+    # appending to output buffer
+    out_buff.append(new_line+"\n")
+
+file.close()
+
+print("saving changes...")
+file = open(os.path.join(os.getcwd(),sys.argv[1]), 'w', encoding=encoding)
+file.writelines(out_buff)
+file.close()
+
+print("done!")
